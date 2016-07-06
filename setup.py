322891dfcc343c6a2fa575714c9c349885b7928b
Tiffany8 <Tiffany8@users.noreply.github.com>
2016-07-06 04:12:34 -0700

    add url to setup.py and change versioning -- from 0.1 to 0.0.1

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 setup(name='SequenceAligner',
-      version='0.1',
+      version='0.0.1',
       author='T Williams',
       author_email='trw.visions@gmail.com',
       description='FASTA sequence aligner.',
+      url='https://github.com/Tiffany8/Sequence-Aligner',
       long_description=readme,
       packages=find_packages(exclude=['tests']),
       entry_points={
