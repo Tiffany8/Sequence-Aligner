f54ef92642e11ec59c3c4e42340987e02e838e01
Tiffany8 <Tiffany8@users.noreply.github.com>
2016-07-06 03:39:55 -0700

    bump version from 0.1-rc11 to 0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 setup(name='SequenceAligner',
-      version='0.1-rc11',
+      version='0.1',
       author='T Williams',
       description='FASTA sequence aligner.',
       long_description=readme,
